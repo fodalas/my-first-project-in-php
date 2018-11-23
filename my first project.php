612b8a0a5a1a29741cb352afbe966dd1ed136c71
fodalas <45221684+fodalas@users.noreply.github.com>
2018-11-23 09:38:18 -0400

    initial

---
+++ my first project.php
@@ -0,0 +1,43 @@
+<?php
+ 
+ echo "<h1> my first project in php </h1>";
+ 
+	$nome = "wanderson";
+	echo $nome;
+	echo "<br>";
+	
+	
+	$numerica = 50; 
+	echo $numerica; 
+	echo "<br>"; 
+	
+
+	$float = 50.99;  
+	echo $float;
+	echo "<br>"; 
+	
+	
+//	$varbolean = true; 
+	//echo $varbolean;
+	echo "<br>"; 
+	
+	
+	//$null = false; 
+	//echo $null;
+	
+	$meu = $float + $numerica;
+	$rei = $meu * 2; 	
+	echo $rei + $numerica;  
+	
+
+/*	help ..
+	echo "<br>";
+	$menor = $meu < 300;   
+    
+	$false = $menor = 1 ;
+	echo $false;
+	
+	result 1 = true
+	result 0 = false 
+					help*/
+?>
